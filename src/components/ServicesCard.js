7069b426fba335d47b9d9e3df686a240b05d3170
Sheezylodhi <muhammadshahzaiblodhi@gmail.com>
2026-01-08 17:58:16 +0500

    in

--- src/components/ServicesCard.js
+++
@@ -1,8 +0,0 @@
-export default function ServicesCard({ title, description }) {
-  return (
-    <div className="border rounded-lg shadow-lg p-6 hover:shadow-xl transition">
-      <h3 className="text-xl font-bold mb-2 text-[#2A4E3B]">{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
